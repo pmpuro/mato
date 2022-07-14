3d961a645ae21128fc2b6bc7ed53a3174c5ba9df
Panu Puro <panu.puro@gmail.com>
2022-07-14 15:35:29 +0300

    Tests for create-coord

--- test/mato/core_test.clj
+++ test/mato/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [mato.core :refer :all]))
 
+(deftest test-create-coord
+  (is (map? (create-coord 1 1)))
+  (is (every? (create-coord 1 1) [:x :y])))
+
 (deftest test-change-coord
   (testing "change-coord creates a hash-map, expecting two maps containing :x and :y"
     (let [x1 1
@@ -27,9 +31,7 @@
       false (collision? [(create-coord 1 1) (create-coord 2 1)])
       false (collision? [(create-coord 0 1) (create-coord 1 1)])
       false (collision? [(create-coord (dec scene-width) 1)])
-      false (collision? [(create-coord 1 (dec scene-height))])
-      )
-    )
+      false (collision? [(create-coord 1 (dec scene-height))])))
   (testing "collision"
     (are [expected result]
       (= expected result)
@@ -40,24 +42,20 @@
 
 (comment
   (will-eat? [(create-coord 1 1)] [(create-coord 0 1)] left)
-  (will-eat? [(create-coord 1 1)] [(create-coord 0 1)] up)
-  )
+  (will-eat? [(create-coord 1 1)] [(create-coord 0 1)] up))
 
 (deftest test-will-eat
   (testing "eating"
     (are [expected result]
       (= expected result)
       true (will-eat? [(create-coord 1 1)] [(create-coord 0 1)] left)
-      true (will-eat? [(create-coord 1 1)] [(create-coord 2 1)] right)
-      )
-    )
+      true (will-eat? [(create-coord 1 1)] [(create-coord 2 1)] right)))
+
   (testing "not eating"
     (are [expected result]
       (= expected result)
       nil (will-eat? [(create-coord 1 1)] [(create-coord 0 1)] right)
-      nil (will-eat? [(create-coord 1 1)] [(create-coord 0 1)] up)
-      )
-    ))
+      nil (will-eat? [(create-coord 1 1)] [(create-coord 0 1)] up))))
 
 (comment
   (run-all-tests))
